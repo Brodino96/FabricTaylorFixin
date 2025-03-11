0ac73bcd01eccc11f0e44d81aca5d3ba55a03edc
Brodino <brodino96@gmail.com>
2025-03-11 17:20:33 +0100

    second initial commit

--- src/main/java/net/brodino/fabrictaylorfixin/mixin/WeaponMasterFix.java
+++ src/main/java/net/brodino/fabrictaylorfixin/mixin/WeaponMasterFix.java
@@ -1,20 +1,40 @@
 package net.brodino.fabrictaylorfixin.mixin;
 
-import com.bawnorton.mixinsquared.TargetHandler;
 import com.minecraftserverzone.weaponmaster.setup.PlayerEntityExtension;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Inject;
 
-@Mixin(value= PlayerEntity.class, priority = 1500)
-public class WeaponMasterFix {
-    @TargetHandler(
-            mixin = "com.minecraftserverzone.weaponmaster.mixin.ClientPlayerMixin",
-            name = "getHotbarSlots"
-    )
 
-    @Inject(method = "@MixinSquared:Handler", at = @At("HEAD"))
-    private void doSomething(CallbackInfo originalCi, CallbackInfo ci) {
-        LOGGER.info("Injecting into handler$######$modid$targetMethod");
+@Mixin(value = PlayerEntity.class, priority = 1500)
+public abstract class WeaponMasterFix implements PlayerEntityExtension {
+
+    public ItemStack hotbarSlot1;
+    public ItemStack hotbarSlot2;
+    public ItemStack hotbarSlot3;
+    public ItemStack hotbarSlot4;
+    public ItemStack hotbarSlot5;
+    public ItemStack hotbarSlot6;
+    public ItemStack hotbarSlot7;
+    public ItemStack hotbarSlot8;
+    public ItemStack hotbarSlot9;
+
+    public ItemStack[] getHotbarSlots() {
+        ItemStack air = new ItemStack(Items.AIR);
+        return new ItemStack[]{
+                this.hotbarSlot1 == null ? air : this.hotbarSlot1,
+                this.hotbarSlot2 == null ? air : this.hotbarSlot2,
+                this.hotbarSlot3 == null ? air : this.hotbarSlot3,
+                this.hotbarSlot4 == null ? air : this.hotbarSlot4,
+                this.hotbarSlot5 == null ? air : this.hotbarSlot5,
+                this.hotbarSlot6 == null ? air : this.hotbarSlot6,
+                this.hotbarSlot7 == null ? air : this.hotbarSlot7,
+                this.hotbarSlot8 == null ? air : this.hotbarSlot8,
+                this.hotbarSlot9 == null ? air : this.hotbarSlot9
+        };
     }
 }
